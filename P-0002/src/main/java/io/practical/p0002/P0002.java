879d862c511834bf9b71c102c81670e22f301dc4
xetienne <xetienne@ITECLUXC1705261.ep.parl.union.eu>
2017-10-27 09:47:08 +0200

    use countdownlatch

--- P-0002/src/main/java/io/practical/p0002/P0002.java
+++ P-0002/src/main/java/io/practical/p0002/P0002.java
@@ -2,51 +2,27 @@ package io.practical.p0002;
 
 import java.io.IOException;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeoutException;
 
 public class P0002 {
 
-	private static final int COUNT = 1_000;
-
 	public static void main(String[] args)
-			throws IOException, InterruptedException, BrokenBarrierException, TimeoutException {
-		// http://tutorials.jenkov.com/java-util-concurrent/cyclicbarrier.html
-		increment();
+	        throws IOException, InterruptedException, BrokenBarrierException, TimeoutException {
+		incrementCountDown();
 	}
 
-	public static void increment() throws InterruptedException, BrokenBarrierException, TimeoutException {
-		// List<IncrementerAction> subtasks = new ArrayList<>();
-		// CyclicBarrier barrier = new CyclicBarrier(COUNT * 4);
-		// IncrementerAction ia1 = new IncrementerAction(1, 0, COUNT, barrier);
-		// IncrementerAction ia2 = new IncrementerAction(1, 2000, COUNT,
-		// barrier);
-		// IncrementerAction ia3 = new IncrementerAction(1, 9999, COUNT,
-		// barrier);
-		// IncrementerAction ia4 = new IncrementerAction(1, 1990, COUNT,
-		// barrier);
-		IncrementerAction ia1 = new IncrementerAction(1, 0, COUNT);
-		IncrementerAction ia2 = new IncrementerAction(1, 2000, COUNT);
-		IncrementerAction ia3 = new IncrementerAction(1, 9999, COUNT);
-		IncrementerAction ia4 = new IncrementerAction(1, 1990, COUNT);
+	public static void incrementCountDown() throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(4);
+		System.out.println("latch count before thread run = " + latch.getCount());
 
-		// subtasks.add(ia1);
-		// subtasks.add(ia2);
-		// subtasks.add(ia3);
-		// subtasks.add(ia4);
-		// ForkJoinPool.commonPool().execute(subtasks);
-		ia1.fork();
-		ia2.fork();
-		ia3.fork();
-		ia4.fork();
-		System.out.println("all tasks invoked");
-		ia1.join();
-		ia2.join();
-		ia3.join();
-		ia4.join();
-		System.out.print("program await ...");
-//		barrier.await();
-		// await(3, TimeUnit.SECONDS);
-		System.out.println("program await ... finished");
+		for (int i = 0; i < 4; i++) {
+			ForkJoinPool.commonPool().execute(new IncrementerThread(i, latch));
+		}
+		System.out.println("latch count before await = " + latch.getCount());
+		latch.await();
+		System.out.println("latch count = " + latch.getCount());
 
 	}
 }
